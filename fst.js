8ed6259a90aae20834e2c1e0332d42bdc20efdfe
MikeCAT <mikecat@users.noreply.github.com>
2023-04-20 04:24:56 +0900

    save language configuration to localStorage

--- fst.js
+++ fst.js
@@ -6,8 +6,38 @@ window.addEventListener("DOMContentLoaded", function() {
 		if (e.id) elems[e.id] = e;
 	});
 
+	const loadLocalStorage = function(key) {
+		try {
+			return localStorage.getItem(key);
+		} catch (e) {
+			return undefined;
+		}
+	};
+	const storeLocalStorage = function(key, value) {
+		try{
+			localStorage.setItem(key, value);
+			return true;
+		} catch (e) {
+			return false;
+		}
+	};
+
+	const LANGUAGE_CONFIG_KEY = "language";
+
+	{
+		let lang = loadLocalStorage(LANGUAGE_CONFIG_KEY);
+		if (!lang) lang = navigator.language.toLowerCase().indexOf("ja") >= 0 ? "ja" : "en";
+		for (let i = 0; i < elems.languageSelect.options.length; i++) {
+			if (elems.languageSelect.options[i].value === lang) {
+				elems.languageSelect.selectedIndex = i;
+				break;
+			}
+		}
+	}
+
 	const updateLanguage = function() {
 		const lang = elems.languageSelect.value;
+		storeLocalStorage(LANGUAGE_CONFIG_KEY, lang);
 		document.body.parentNode.setAttribute("lang", lang);
 		const selects = document.getElementsByTagName("select");
 		for (let i = 0; i < selects.length; i++) {
